c41e0bc6d57eb01aa0301906078d36b8b376d50b
ACSoverride <58186282+ACSoverride@users.noreply.github.com>
2023-02-22 23:55:15 -0500

    moving stuff from firebase attempt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Alert } from 'flowbite-svelte';
+	import { Blockquote } from 'flowbite-svelte'
+	import '/src/styles.css'
+	import {  Heading, P, A, Mark, Secondary } from 'flowbite-svelte'
+</script>
+<div class="thebg"></div>
+<div class="p-8">
+	<div class="centertext">
+		<h1>Welcome to Mana Rock!</h1>
+	</div>
+</div>
