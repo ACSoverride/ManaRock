841de904b779b6c3f40a7b4abf490425890c8d02
ACSoverride <mattinhvt@gmail.com>
2023-02-24 11:03:58 -0500

    restyled things and added an accordion to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,41 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import { AccordionItem, Alert } from 'flowbite-svelte';
 	import { Blockquote } from 'flowbite-svelte'
 	import '/src/styles.css'
-	import {  Heading, P, A, Mark, Secondary } from 'flowbite-svelte'
+	import {  Heading, P, A, Mark, Secondary, Hr, Accordion, ActivityItem} from 'flowbite-svelte'
 </script>
 <div class="thebg"></div>
 <div class="p-8">
 	<div class="centertext">
 		<h1>Welcome to Mana Rock!</h1>
 	</div>
-	<div class="p1">
-		<p>ManaRock is a svelte based app to help you manage your Magic the Gathering collection.
+	<P class="p1" align="center">ManaRock is a svelte based app to help you manage your Magic the Gathering collection.
 		Designed to be both beautiful and functional, ManaRock makes managing your cards a breeze.
-		</p>
+	</P>
+	<div class="a1">
+		<Hr width="w-48" height="h-1"></Hr>
+		<Accordion>
+			<AccordionItem>
+				<span slot="header">Speed</span>
+				<P class="p2" align="center">
+					Designed with Svelte ManaRock is fast and optimized, giving
+					you the best experience possible
+				</P>
+			</AccordionItem>
+			<AccordionItem>
+				<span slot="header">API</span>
+				<P class="p2" align="center">
+					ManaRock uses the Scryfall API allowing access to almost any card you can
+					think of! No card is left behind 
+				</P>
+			</AccordionItem>
+			<AccordionItem>
+				<span slot="header">For players by a player</span>
+				<P class="p2" align="center">
+					ManaRock is built for Magic players by a Magic player. The site will remain completely free for all users.
+					The code is 100% open source and liscenced under the GPLv3
+				</P>
+			</AccordionItem>
+		</Accordion>
 	</div>
 </div>
