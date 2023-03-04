ab2d01ef33f9d1457cd98fa568f05568285901cb
ACSoverride <58186282+ACSoverride@users.noreply.github.com>
2023-03-04 18:57:34 -0500

    attempt to fix kv call

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,9 @@
 	import { Blockquote } from 'flowbite-svelte'
 	import '/src/styles.css'
 	import {  Heading, P, A, Mark, Secondary, Hr, Accordion, ActivityItem} from 'flowbite-svelte'
-	async function setKV(){
-		await MainTest.put("TestKey", "TestValue");
+	export async function onRequest(context) {
+		await context.env.MainTest.put("TestKey", "TestValue");
 	}
-	setKV();
 </script>
 <div class="thebg"></div>
 <div class="p-8">
