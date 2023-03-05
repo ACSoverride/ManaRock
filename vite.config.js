ed196a4726800d079bf6f0005542b4c2acbf445d
ACSoverride <58186282+ACSoverride@users.noreply.github.com>
2023-03-04 19:34:29 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
 	build: {
-    		rollupOptions: {
-      			external: [
-        			"setKV.js",
-      ],
-    },
-  },
+    	rollupOptions: {
+      		external: [
+        		"setKV.js",
+      		],
+    	},
+  	},
 });
